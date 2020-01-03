979c1f25f53de4608930a18a2b3624fb95c1960c
goonerholic <goonerholic@gmail.com>
2020-01-04 00:56:01 +0900

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const candleEmitter = require('./lib/candleEmitter')
-
-const client = candleEmitter({
-	symbols: ['XBTUSD'],
-	timeframe: 1,
-	testnet: false
-})
-
-client.on('candle', candle => console.log(candle))
